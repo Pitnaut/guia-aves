8c22539590832a19311b5932572bd989694be758
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2023-12-28 16:51:11 +0100

    Add a loading screen before show all birds

--- src/components/BirdList.jsx
+++ src/components/BirdList.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { birds } from '../data/data';
 import BirdCard from './BirdCard';
 import '../stylesheets/BirdList.css';
 
 const BirdList = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000); // ajusta este tiempo según sea necesario
+
+    return () => clearTimeout(timer); // limpia el temporizador si el componente se desmonta
+  }, []);
+
   return (
     <div className='bird-list'>
-      {birds.map((bird) => {
-        return (
+      {loading ? (
+        <div className='loader'>Cargando...</div> // reemplaza esto con tu propio spinner o cargador
+      ) : (
+        birds.map((bird) => {
+          return (
             <BirdCard 
               bird={bird} 
               className={({ isActive }) => isActive ? 'active-bird' : 'bird'}
-              key={bird.nombre}        />
-        );
-      })}
+              key={bird.nombre}        
+            />
+          );
+        })
+      )}
     </div>
   )
 }
