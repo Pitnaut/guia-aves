5eab15a87a8a25734cc0753e055ed60c5b00f7a3
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2024-03-15 17:36:03 +0100

    First version of the main home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
 import logo from '../assets/logo.png';
+import petirrojo from '../../public/images/Petirrojo_europeo.png';
 import '../stylesheets/Home.css'
 
 export default function Home() {
@@ -9,9 +10,27 @@ export default function Home() {
   }, []);
 
   return (
-    <div className='home' >
-      <img src={logo} />
-      <h1>Bienvenido a la guía de Aves de Madrid</h1>
-    </div>
+      <div className='home' >
+        <h2>Bienvenido a la guía de Aves de Madrid</h2>
+        <div className='intro-logo'>      
+          <img src={logo} />
+          <p>En esta app podrás buscar información básica sobre las aves que habitan en la Comunidad de Madrid.</p>
+        </div>
+        <div className='main-text'>
+          <p>
+            En esta guía no oficial con las principales aves que habitan la Comunidad de Madrid,
+            tendrás una información sencilla sobre cada ave.
+          </p>
+          <img src={petirrojo} />
+        </div>
+        <div className='big-text'>
+          <p>
+            Si quieres saber más sobre alguna especie, puedes visitar la web de SEO Birdlife o Wikipedia, donde encontrarás información más detallada.
+          </p>
+          <p>Si tienes alguna duda sobre como se usa esta app, tienes dentro de 'Guía de uso' las instrucciones y la leyenda que aparece en los detalles del ave.</p>
+          <p>En caso de encontrar algún error o sencillamente tienes una sugerencia sobre como mejorar puedes ponerte en contacto con el correo proporcionado en el footer.</p>
+          <p>Esta aplicación es totalmente gratuita y forma parte de un proyecto educativo personal. Se han dado crédito a los autores de las fotos.</p>
+        </div>
+      </div>
   )
 }
